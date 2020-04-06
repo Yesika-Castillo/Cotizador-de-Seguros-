1850ff4330fafc5f79800c2436302eb58cd981f3
Yesika Castillo <ylcastillo29@hotmail.com>
2020-04-06 18:47:27 -0500

    se creo styled, components,formulario,calculo basado en carateristicas propTypes animacion y ajustes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Components/Header'
+import Formulario from './Components/Formulario'
+import Resumen from './Components/Resumen'
+import Resultado from './Components/Resultado'
+import Spinner from './Components/Spinner'
+
 import styled from '@emotion/styled'
 
+const Contenedor = styled.div`
+  max-width: 600;
+  margin: 0 auto;
+`
+const ContenedorFormulario = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`
+
 function App() {
-  return <Header titulo="Cotizador de Seguros" />
+  const [resumen, guardarResumen] = useState({
+    cotizacion: 0,
+    datos: {
+      marca: '',
+      year: '',
+      plan: ''
+    }
+  })
+
+  const [cargando, guardarCargando] = useState(false)
+
+  //extraer datos
+  const { cotizacion, datos } = resumen
+
+  return (
+    <Contenedor>
+      <Header titulo="Cotizador de Seguros" />
+
+      <ContenedorFormulario>
+        <Formulario
+          guardarResumen={guardarResumen}
+          guardarCargando={guardarCargando}
+        />
+
+        {cargando ? <Spinner /> : null}
+
+        <Resumen datos={datos} />
+
+        {!cargando ? <Resultado cotizacion={cotizacion} /> : null}
+      </ContenedorFormulario>
+    </Contenedor>
+  )
 }
 
 export default App
